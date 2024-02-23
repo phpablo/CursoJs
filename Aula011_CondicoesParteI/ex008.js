460bb1da77012360580137c2dbb4d440d59c681a
phpablo <pablohenrique120@gmail.com>
2024-02-23 11:27:43 -0300

    :star: Add file ex008 - Add file exercices about first if

--- Aula011_CondicoesParteI/ex008.js
+++ Aula011_CondicoesParteI/ex008.js
@@ -1,5 +1,7 @@
-var vel = 12;
+var vel = 61;
 
 console.log(`A velocidade do seu carro é ${vel} km/h`);
-console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+if (vel > 60) {
+  console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+}
 console.log(`Dirija sempre com o cinto de segurança!`);
