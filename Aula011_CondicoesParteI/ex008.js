8ef97a6568b10cdc43a899e21f5dacca14e7c847
phpablo <pablohenrique120@gmail.com>
2024-02-23 11:14:49 -0300

    :star: Add new file to using js condition
    -File to try and study conditions

---
+++ Aula011_CondicoesParteI/ex008.js
@@ -0,0 +1,5 @@
+var vel = 12;
+
+console.log(`A velocidade do seu carro é ${vel} km/h`);
+console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+console.log(`Dirija sempre com o cinto de segurança!`);
