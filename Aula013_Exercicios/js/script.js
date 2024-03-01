a6f894e8fda19224c54e97efd4fee228d3e373a2
phpablo <pablohenrique120@gmail.com>
2024-03-01 11:47:46 -0300

    :memo: Add comentários ao projeto

--- Aula013_Exercicios/js/script.js
+++ Aula013_Exercicios/js/script.js
@@ -8,12 +8,15 @@ function carregar() {
   msg.innerHTML = `Agora são ${hora} horas.`;
 
   if(hora >=0 && hora < 12) {
+    // BOM DIA
     img.src = 'imagens/manha.png';
     document.body.style.background = '#c0cdc8';
   }else if( hora >=12 && hora < 18){
+    // BOA TARDE
     img.src = 'imagens/tarde.png';
     document.body.style.background = '#e7bb59';
   }else{
+    // BOA NOITE
     img.src = 'imagens/noite.png';
     document.body.style.background = '#11202c';
   }
