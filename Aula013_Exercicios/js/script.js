ea31ce8918fa2f649690ca60e92119ede2159d81
phpablo <pablohenrique120@gmail.com>
2024-03-01 11:46:55 -0300

    :rocket: Adiciona projeto do site do Bom dia
    - Add arquivo do projeto
    - Script para modificar imagem
    - script para modificar a cor de fundo
    - função para pegar hora atual do sistema.

--- Aula013_Exercicios/js/script.js
+++ Aula013_Exercicios/js/script.js
@@ -1,1 +1,20 @@
-alert('Eai seus putos!')
+function carregar() {
+  var msg = window.document.getElementById('msg');
+  var img = window.document.getElementById('imagem');
+  var data = new Date();
+  // var hora = data.getHours();
+  var hora = 9;
+
+  msg.innerHTML = `Agora são ${hora} horas.`;
+
+  if(hora >=0 && hora < 12) {
+    img.src = 'imagens/manha.png';
+    document.body.style.background = '#c0cdc8';
+  }else if( hora >=12 && hora < 18){
+    img.src = 'imagens/tarde.png';
+    document.body.style.background = '#e7bb59';
+  }else{
+    img.src = 'imagens/noite.png';
+    document.body.style.background = '#11202c';
+  }
+}
